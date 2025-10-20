3a56134acd1d23e4ea07ba755932386a953307fc
tarekkazti <158314101+tarekkazti@users.noreply.github.com>
2025-10-20 08:01:29 +0100

    Create _middleware.js

---
+++ pages/_middleware.js
@@ -0,0 +1,13 @@
+import { NextResponse } from "next/server";
+
+export function middleware(req) {
+  const res = NextResponse.next();
+
+  res.headers.set("X-Frame-Options", "ALLOW-FROM *");
+  res.headers.set(
+    "Content-Security-Policy",
+    "frame-ancestors * https://* http://*;"
+  );
+
+  return res;
+}
