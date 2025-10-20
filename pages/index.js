72042ed592f06dd65b73528c8dd210b03f3e29fd
tarekkazti <158314101+tarekkazti@users.noreply.github.com>
2025-10-20 07:52:33 +0100

    Delete pages directory

--- pages/index.js
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return null;
-}
-
-export async function getServerSideProps({ res }) {
-  res.writeHead(302, { Location: "/index.html" });
-  res.end();
-  return { props: {} };
-}
