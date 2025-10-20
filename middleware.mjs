07c898abeeb2722a9bc8a88e8a762a2505daf4ac
tarekkazti <158314101+tarekkazti@users.noreply.github.com>
2025-10-20 07:47:34 +0100

    Update middleware.mjs

--- middleware.mjs
+++ middleware.mjs
@@ -3,10 +3,10 @@ import { NextResponse } from "next/server";
 export function middleware(req) {
   const res = NextResponse.next();
 
-  res.headers.set("X-Frame-Options", "ALLOWALL");
+  res.headers.set("X-Frame-Options", "ALLOW-FROM *");
   res.headers.set(
     "Content-Security-Policy",
-    "frame-ancestors * 'self' https://*"
+    "frame-ancestors 'self' https://* http://* *;"
   );
 
   return res;
